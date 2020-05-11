b48598e7b811e84ffefa013bb0ca11b0794c481d
Nikolay Bobkov <Zeal0t89@yandex.ru>
2020-05-11 16:21:12 +0300

    fix: мелкие правки стилей, разметки и JS

--- script.js
+++ script.js
@@ -1,42 +1,31 @@
-const page = document.querySelector(".page");
-const content = page.querySelector(".content");
-const profile = content.querySelector(".profile");
+const profile = document.querySelector(".profile");
 const profileInfo = profile.querySelector(".profile__info");
 const editButton = profileInfo.querySelector(".profile__edit-button");
-const popup = content.querySelector(".popup");
+const popup = document.querySelector(".popup");
 const popupToggle = popup.querySelector(".popup__toggle");
+
 let profileName = profileInfo.querySelector(".profile__title");
 let profileActiviti = profileInfo.querySelector(".profile__subtitle");
 let nameInput = popup.querySelector(".popup__input_name");
 let activitiInput = popup.querySelector(".popup__input_activiti");
+let popupForm = popup.querySelector(".popup__form");
+const saveButton = popupForm.querySelector(".popup__button");
 
-nameInput.value = profileName.textContent;
-activitiInput.value = profileActiviti.textContent;
-
-function openPopup() {
-  popup.setAttribute("enable", true);
+function openClosePopup() {
   popup.classList.toggle("popup_active");
-  nameInput.value = profileName.textContent;
-  activitiInput.value = profileActiviti.textContent;
+  if (popup.classList.contains("popup_active")) {
+    nameInput.value = profileName.textContent;
+    activitiInput.value = profileActiviti.textContent;
+  }
 }
-
-editButton.addEventListener("click", openPopup);
-function closePopup() {
-  popup.setAttribute("enable", true);
-  popup.classList.toggle("popup_active");
-  nameInput.textContent = profileName.value;
-  activitiInput.textContent = profileActiviti.value;
-}
-popupToggle.addEventListener("click", openPopup);
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileActiviti.textContent = activitiInput.value;
+  openClosePopup();
 }
-
-let popupForm = content.querySelector(".popup__form");
+editButton.addEventListener("click", openClosePopup);
 popupForm.addEventListener("submit", formSubmitHandler);
-
-const saveButton = popupForm.querySelector(".popup__button");
-saveButton.addEventListener("click", closePopup);
+popupToggle.addEventListener("click", openClosePopup);
+saveButton.addEventListener("click", formSubmitHandler);
